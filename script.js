9b08dff7a7737551e277b5a96a0b7f7b5664a2f5
montremoi <146775516+montremoi@users.noreply.github.com>
2023-10-03 23:01:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const showMoreButtons = document.querySelectorAll('.showMoreButton');
-    
-    showMoreButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const infosParagraph = this.parentElement.querySelector('.infos');
-            
-            if (infosParagraph.style.display === 'none' || infosParagraph.style.display === '') {
-                infosParagraph.style.display = 'block';
-                this.textContent = '-';
-                infosParagraph.scrollIntoView({ behavior: 'smooth' });
-            } else {
-                infosParagraph.style.display = 'none';
-                this.textContent = '+';
-            }
+ document.addEventListener('DOMContentLoaded', function () {
+        const showMoreButtons = document.querySelectorAll('#showMoreButton');
+        
+        showMoreButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                const infosParagraph = this.parentElement.querySelector('.infos');
+                
+                if (infosParagraph.style.display === 'none' || infosParagraph.style.display === '') {
+                    infosParagraph.style.display = 'block';
+                    this.textContent = '-';
+                    infosParagraph.scrollIntoView({ behavior: 'smooth' });
+                } else {
+                    infosParagraph.style.display = 'none';
+                    this.textContent = '+';
+                }
+            });
         });
     });
-});
