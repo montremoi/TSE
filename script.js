6956ad2d4fce0a9a06d0be5555adde3dbfffe4b6
montremoi <146775516+montremoi@users.noreply.github.com>
2023-10-04 17:27:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function showMoreLess() {
+    showMoreButtons.forEach((button, index) => {
+    button.addEventListener('click', function() {
+        window.alert('Button cliqué')
+        if (infosElements[index].style.display === 'block') {
+            infosElements[index].style.display = 'none';
+            button.textContent = '+';
+        } else {
+            infosElements[index].style.display = 'block';
+            button.textContent = '-';
+        }
+    });
+});
+});
+
