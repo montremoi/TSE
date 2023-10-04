5c159acee81f9d47a38c75aac886acc63434e329
montremoi <146775516+montremoi@users.noreply.github.com>
2023-10-04 20:18:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.alert("1")
+//window.alert("2")
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
