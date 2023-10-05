c537c1cac2ccdc04a247c52e99907fc40f6989ff
montremoi <146775516+montremoi@users.noreply.github.com>
2023-10-05 09:05:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -86,3 +86,32 @@ document.querySelectorAll('.showMoreButton').forEach(button => {
     });
 });
 */
+        
+const showMoreButtons = document.querySelectorAll('.showMoreButton');
+const infosElements = document.querySelectorAll('.infos');
+
+    showMoreButtons.forEach((button, index) => {
+        button.addEventListener('click', function() {
+            if (infosElements[index].style.display === 'block') {
+                infosElements[index].style.display = 'none';
+                button.textContent = '+';
+                } else {
+                    infosElements[index].style.display = 'block';
+                    button.textContent = '-';
+                }
+            });
+        });
+
+      
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function(e) {
+                e.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+            });
+        });
